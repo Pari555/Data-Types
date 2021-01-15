6eb86eacd031b0461f9a78c150247a488ffe5d77
Debanshi123050 <123050@eesd.org>
2021-01-15 00:17:27 +0000

    added code

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ Age = "12" #string
 FaveNumber = 30 #integer
 FaveChar = "P" #character
 isHunger = True #boolean
+
+wantPizza = False
+
+print('FirstName')
+print("Hello")
+
+print(FavNumber)
+
+print(FaveChar)
+
+print(FavNumber)
